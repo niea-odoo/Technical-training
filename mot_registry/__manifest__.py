1efe2f44bca21805c81015d295e2e36f7c6edcbf
niea <niea@odoo.com>
2023-07-18 10:48:49 -0600

    Initial commit with Registry module manifest

---
+++ mot_registry/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': 'Motorcycle Registry\n====================\nThis Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.',
+    'author': 'niea-odoo',
+    'website': 'my github repo',
+    'category': 'Kawiil/Stuff',
+
+    'application': True, 
+
+}
