6c806cf1ee1357a76c2bc2acff63977feea31c9b
niea <niea@odoo.com>
2023-07-19 15:32:41 -0600

    Adding constrains for fields as per TE09

--- mot_registry/models/motorcycle_registry.py
+++ mot_registry/models/motorcycle_registry.py
@@ -1,5 +1,8 @@
 from odoo import models as dbstuff
 from odoo import fields as dbstuffbutsmaller
+from odoo import api
+from odoo.exceptions import ValidationError
+import re
 
 class Motorcycle_registry(dbstuff.Model):
     """Registry instance for a motorcycle 
@@ -25,3 +28,16 @@ class Motorcycle_registry(dbstuff.Model):
     certificate_title = dbstuffbutsmaller.Binary()
 
     register_date = dbstuffbutsmaller.Date()
+
+    @api.constrains('license_plate')
+    def _check_license_plate(self):
+        for motorcycle_registry in self:
+            #{2A}{2}{2}({2A})
+            if(motorcycle_registry.license_plate  ):
+                raise ValidationError("License plates must conform to defined rules")
+
+    @api.constrains('vin')
+    def _check_vin(self):
+        for motorcycle_registry in self:
+            if(motorcycle_registry.vin ... ):
+                raise ValidationError("VIN must conform to defined rules")
