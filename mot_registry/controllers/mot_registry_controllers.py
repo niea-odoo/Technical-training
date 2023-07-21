c875054c3a6fb2020f4411c3c69fc7354edc0c64
Narciso Isaac Eugenio Aceves <niea@Narcisos-MacBook-Air.local>
2023-07-21 16:41:20 -0600

    Improving webpage, working on web design to match TE12 roughly and load images

--- mot_registry/controllers/mot_registry_controllers.py
+++ mot_registry/controllers/mot_registry_controllers.py
@@ -7,11 +7,7 @@ class Registry(http.Controller):
     
     @http.route('/compare/', auth='public', website=True)
     def compare(self, **kw):
-        motorcycles = http.request.env['product.template'].search([('detailed_type','=','Motorcycle')])
+        motorcycles = http.request.env['product.template'].search([('detailed_type','=','motorcycle')])
         return http.request.render('mot_registry.website', {
             'motorcycles':motorcycles,
         })
-    
-    @http.route('/motorcycles/<model("product.template"):model>/', auth='public', website=True)
-    def motorcycles(self, motorcycles):
-        return http.request.render('product.product.template')
