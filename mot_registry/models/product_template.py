a36bf322e9cedd0ff78def3dd155e5566c7f6234
Narciso Isaac Eugenio Aceves <niea@Narcisos-MacBook-Air.local>
2023-07-21 11:43:50 -0600

    Working version, TE11 finished... finally

--- mot_registry/models/product_template.py
+++ mot_registry/models/product_template.py
@@ -1,9 +1,9 @@
-from odoo import models
-from odoo import fields
+from odoo import models as dbstuff
+from odoo import fields as dbstuffbutsmaller
 
-class ProductTemplate(models.Model):
+class ProductTemplate(dbstuff.Model):
     _inherit = 'product.template'
-    detailed_type = fields.Selection(selection_add=[
+    detailed_type = dbstuffbutsmaller.Selection(selection_add=[
         ('motorcycle', 'Motorcycle'),
     ], ondelete={'motorcycle': 'set product'})
 
